c4e91ddcc5f17a775b85462f548917e912946864
SoKyungHyun <valentine92exp@gmail.com>
2024-06-17 15:53:45 +0900

    Refactoring: profile, notification page

--- front_web/src/app/pages/notification/notification.component.ts
+++ front_web/src/app/pages/notification/notification.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PageLayoutComponent } from '../../page-layout/page-layout.component';
 
 @Component({
   selector: 'app-notification',
   standalone: true,
-  imports: [],
+  imports: [PageLayoutComponent],
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.less'
 })
--- front_web/src/app/pages/profile/profile.component.ts
+++ front_web/src/app/pages/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PageLayoutComponent } from '../../page-layout/page-layout.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [PageLayoutComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.less'
 })
