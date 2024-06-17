c8873b192e30cb7efb7ee5d772a055583f7358c3
SoKyungHyun <valentine92exp@gmail.com>
2024-06-17 15:47:31 +0900

    header edit: profile button, alarm button

--- front_web/src/app/components/header/header.component.ts
+++ front_web/src/app/components/header/header.component.ts
@@ -2,15 +2,21 @@ import { Component } from '@angular/core';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NzMenuModule, RouterModule, NzButtonModule],
+  imports: [NzBadgeModule, NzAvatarModule,NzIconModule,NzFlexModule,NzGridModule,NzMenuModule, RouterModule, NzButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
 export class HeaderComponent {
   showNavigate: boolean = true;
+  count : number = 1;
 
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
