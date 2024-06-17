14ffc48d78604252e5cb0ba9354ed8027394914d
SoKyungHyun <valentine92exp@gmail.com>
2024-06-17 17:13:55 +0900

    edit profile card

--- front_web/src/app/profile-card/profile-card.component.ts
+++ front_web/src/app/profile-card/profile-card.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { NzCardComponent } from 'ng-zorro-antd/card';
 import { Member } from '../interfaces/member';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormControl, FormGroup, FormsModule, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
-  imports: [NzCardComponent],
+  imports: [FormsModule, NzCardComponent, NzFormModule,CommonModule, NzButtonModule],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.less'
 })
@@ -16,5 +20,23 @@ export class ProfileCardComponent {
     email: 'valentine@cywell.co.kr',
     hireDate: new Date(2022,3,3),
     isActivate: true
+  }
+
+  validateForm: FormGroup<{
+    password: FormControl<string>;
+  }> = this.fb.group({
+    password: ['', [Validators.required]],
+  });
+
+  password : string = ''
+
+  constructor(private fb: NonNullableFormBuilder) {}
+
+  deactivate(){
+    alert('퇴사 처리')
+  }
+
+  submitEdit = () => {
+    alert('변경'+ this.validateForm.value.password)
   }
 }
