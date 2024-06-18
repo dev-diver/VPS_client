58d78e612d0c052aa55cc865d76923c30b2e8371
SoKyungHyun <valentine92exp@gmail.com>
2024-06-18 11:12:31 +0900

    Modal and date picker

--- front_web/src/app/components/modal-add-button/modal-add-button.component.ts
+++ front_web/src/app/components/modal-add-button/modal-add-button.component.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-modal-add-button',
   standalone: true,
-  imports: [NzButtonModule, NzIconModule],
+  imports: [FormsModule, CommonModule, NzDatePickerModule, NzButtonModule, NzIconModule, NzModalModule],
   templateUrl: './modal-add-button.component.html',
   styleUrl: './modal-add-button.component.less'
 })
 export class ModalAddButtonComponent {
+  isVisible = false;
+  isOkLoading = false;
+  dateRange: Date[] = [];
 
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    this.isOkLoading = true;
+    setTimeout(() => {
+      this.isVisible = false;
+      this.isOkLoading = false;
+    }, 3000);
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+  onChange(result: Date[]): void {
+    console.log('Selected Time: ', result);
+  }
+
+  onOk(result: Date | Date[] | null): void {
+    console.log('onOk', result);
+  }
+
+  onCalendarChange(result: Array<Date | null>): void {
+    console.log('onCalendarChange', result);
+  }
 }
