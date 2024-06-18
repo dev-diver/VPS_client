019b988b9aa02a0e4d570de7f778b4005a37ed51
SoKyungHyun <valentine92exp@gmail.com>
2024-06-18 13:31:09 +0900

    add button to group manage card

--- front_web/src/app/components/group-manage-card/group-card/group-card.component.ts
+++ front_web/src/app/components/group-manage-card/group-card/group-card.component.ts
@@ -5,11 +5,12 @@ import { NameTagComponent } from '../../name-tag/name-tag.component';
 import { GroupTagComponent } from '../../group-tag/group-tag.component';
 import { Group } from '../../../interfaces/group';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { ModalAddButtonComponent } from '../../modal-add-button/modal-add-button.component';
 
 @Component({
   selector: 'app-group-card',
   standalone: true,
-  imports: [NzListModule, NzGridModule, NameTagComponent, NzListModule, GroupTagComponent, NzCardModule],
+  imports: [ModalAddButtonComponent,NzListModule, NzGridModule, NameTagComponent, NzListModule, GroupTagComponent, NzCardModule],
   templateUrl: './group-card.component.html',
   styleUrl: './group-card.component.less'
 })
