3c35a3b0f7b4e12fede5871b958c1a380da32539
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-06-23 10:58:44 +0900

    make default vacation plan object

--- front_web/src/app/components/dashboard-vacation-plan-card/dashboard-vacation-plan-card.component.ts
+++ front_web/src/app/components/dashboard-vacation-plan-card/dashboard-vacation-plan-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { DashboardVacationCardComponent } from '../dashboard-vacation-card/dashboard-vacation-card.component';
-import { VacationPlan } from '../../interfaces/vacation-plan';
+import { VacationPlan, createDefaultVacationPlan } from '../../interfaces/vacation-plan';
 
 @Component({
   selector: 'app-dashboard-vacation-plan-card',
@@ -11,22 +11,5 @@ import { VacationPlan } from '../../interfaces/vacation-plan';
   styleUrl: './dashboard-vacation-plan-card.component.less'
 })
 export class DashboardVacationPlanCardComponent {
-  @Input() vacationPlanData: VacationPlan = {
-    id: 0,
-    member_id: 0,
-    member_name: '',
-    apply_date: new Date(),
-    approve_date: new Date(),
-    vacations: [
-      {
-        teamName: '',
-        name: '',
-        startDatetime : new Date(),
-        endDatetime : new Date(),
-        process: true
-      }
-    ],
-    process_state: 0,
-    cancel_state: 0
-  }
+  @Input() vacationPlanData: VacationPlan = createDefaultVacationPlan();
 }
--- front_web/src/app/interfaces/vacation-plan.ts
+++ front_web/src/app/interfaces/vacation-plan.ts
@@ -11,3 +11,24 @@ export interface VacationPlan {
   process_state: number,
   cancel_state: number
 }
+
+export function createDefaultVacationPlan(): VacationPlan {
+  return {
+    id: 0,
+    member_id: 0,
+    member_name: '',
+    apply_date: new Date(),
+    approve_date: new Date(),
+    vacations: [
+      {
+        teamName: '',
+        name: '',
+        startDatetime: new Date(),
+        endDatetime: new Date(),
+        process: true
+      }
+    ],
+    process_state: 0,
+    cancel_state: 0
+  };
+}
