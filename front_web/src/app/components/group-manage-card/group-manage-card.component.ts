8683e1fe0c50eeadca3716b8fc6b35a73c55a5cb
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-06-23 09:40:34 +0900

    edit group-manage-card

--- front_web/src/app/components/group-manage-card/group-manage-card.component.ts
+++ front_web/src/app/components/group-manage-card/group-manage-card.component.ts
@@ -1,36 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NzCardComponent } from 'ng-zorro-antd/card';
 import { GroupAddCardComponent } from './group-add-card/group-add-card.component';
 import { GroupCardComponent } from './group-card/group-card.component';
 import { Group } from '../../interfaces/group';
 import { FoldingCardComponent } from '../folding-card/folding-card.component';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { CompanyService } from '../../services/company.service';
 
 @Component({
   selector: 'app-group-manage-card',
   standalone: true,
-  imports: [NzCardComponent, GroupAddCardComponent, GroupCardComponent, FoldingCardComponent],
+  imports: [NzGridModule, NzListModule,  NzCardComponent, GroupAddCardComponent, GroupCardComponent, FoldingCardComponent],
   templateUrl: './group-manage-card.component.html',
   styleUrl: './group-manage-card.component.less'
 })
 export class GroupManageCardComponent {
-  group : Group = {
-    name : '개발팀',
-    color : '#ffffff',
-    members: [
-      {
-        name: '소경현',
-        companyName: '싸이웰 시스템',
-        email: 'valentine@cywell.co.kr',
-        hireDate: new Date(2024,5,3),
-        isActivate: true
-      },
-      {
-        name: '김장겸',
-        companyName: '싸이웰 시스템',
-        email: 'jk@cywell.co.kr',
-        hireDate: new Date(2022,3,3),
-        isActivate: true
-      }
-    ]
+
+  @Input() companyId : number = 0
+  groups : Group[] = []
+
+  constructor(private companyService: CompanyService) {}
+
+  ngOnInit() {
+    this.companyService.getCompanyGroups(this.companyId).then((data) => {
+      this.groups = data;
+    })
   }
 }
