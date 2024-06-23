84fc97d5a14fd538c96f3d8fc3d40e643d183d3e
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-06-23 17:16:26 +0900

    convert to vacation card

--- front_web/src/app/components/dashboard-vacation-plan-card/dashboard-vacation-plan-card.component.ts
+++ front_web/src/app/components/dashboard-vacation-plan-card/dashboard-vacation-plan-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { DashboardVacationCardComponent } from '../dashboard-vacation-card/dashboard-vacation-card.component';
 import { VacationPlan, createDefaultVacationPlan } from '../../interfaces/vacation-plan';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 @Component({
   selector: 'app-dashboard-vacation-plan-card',
   standalone: true,
-  imports: [NzListModule, DashboardVacationCardComponent],
+  imports: [NzCardModule, NzListModule, DashboardVacationCardComponent],
   templateUrl: './dashboard-vacation-plan-card.component.html',
   styleUrl: './dashboard-vacation-plan-card.component.less'
 })
