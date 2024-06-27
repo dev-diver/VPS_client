aa41d2136f95350410cb745aef88a4aa6d804c40
SoKyungHyun <valentine92exp@gmail.com>
2024-06-27 15:32:42 +0900

    implement login, logout with token

--- front_web/src/app/services/auth.service.ts
+++ front_web/src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import { AxiosInstance } from 'axios';
 import { AxiosInstanceService } from './axios-instance.service';
 import { Auth } from '../interfaces/auth';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 
 @Injectable({
@@ -11,22 +13,29 @@ import { Router } from '@angular/router';
 export class AuthService {
 
   private axiosInstance: AxiosInstance;
-  public auth: Auth = {} as Auth;
+  private auth: Auth = {} as Auth;
   public isLoggedIn = false;
+  private jwtHelper :JwtHelperService = new JwtHelperService();
 
-  constructor(private router: Router, ) {
+  constructor(private router: Router, private cookieService : CookieService) {
     this.axiosInstance = new AxiosInstanceService().getAxiosInstance()
+    
   }
 
   async login(email: string, password: string): Promise<void> {
-    const response = await this.axiosInstance.post('/auth/login', { email, password });
-    this.setAuth(response.data)
-    this.router.navigate(['/calendar'])
-  }
-
-  logout() {
-    this.clearAuth();
-
+    try{
+      const response = await this.axiosInstance.post('/auth/login', { email, password });
+      const token = this.cookieService.get('authToken_info')
+      if(token){
+        const decodedToken = this.jwtHelper.decodeToken(token)
+        this.setAuth(decodedToken)
+        this.router.navigate(['/calendar'])
+      }else{
+        throw new Error('로그인 실패')
+      }
+    }catch(e){
+      console.log(e)
+    }
   }
 
   private setAuth(auth: Auth) {
@@ -34,7 +43,18 @@ export class AuthService {
     this.isLoggedIn = true;
   }
 
+  public getAuth(): Auth {
+    return this.auth;
+  }
+
+  async logout(): Promise<void> {
+    await this.axiosInstance.post('/auth/logout');
+    this.clearAuth();
+    this.router.navigate(['/login']);
+  }
+
   private clearAuth() {
+    this.cookieService.delete('authToken_info')
     this.auth = {} as Auth;
     this.isLoggedIn = false;
   }
