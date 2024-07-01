5256c8ea24e623b7b7af8e141e3b81ff16e20e41
SoKyungHyun <valentine92exp@gmail.com>
2024-07-01 10:29:55 +0900

    insert organizeMap to admin

--- front_web/src/app/pages/admin/admin.component.ts
+++ front_web/src/app/pages/admin/admin.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { CompanyManageCardComponent } from '../../components/company-manage-card/company-manage-card.component';
 import { CompanyMembersCardComponent } from '../../components/company-members-card/company-members-card.component';
-import { GroupManageCardComponent } from '../../components/group-manage-card/group-manage-card.component';
 import { ModalButtonComponent } from '../../components/modal-button/modal-button.component';
 import { PageLayoutComponent } from '../../page-layout/page-layout.component';
 import { FoldingCardComponent } from '../../components/folding-card/folding-card.component';
 import { CompanyCardComponent } from '../../components/company-card/company-card.component';
 import { Auth } from '../../interfaces/auth';
 import { AuthService } from '../../services/auth.service';
+import { OrganizeMapComponent } from '../../components/organize-map/organize-map.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [CompanyCardComponent, FoldingCardComponent,PageLayoutComponent,CompanyManageCardComponent, CompanyMembersCardComponent, GroupManageCardComponent, ModalButtonComponent],
+  imports: [CompanyCardComponent, FoldingCardComponent,PageLayoutComponent, CompanyManageCardComponent, CompanyMembersCardComponent, OrganizeMapComponent, ModalButtonComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.less'
 })
