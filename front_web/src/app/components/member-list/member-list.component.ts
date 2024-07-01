f564aecfe29043eaa41aa2759cb3803310b5c1de
SoKyungHyun <valentine92exp@gmail.com>
2024-07-01 16:22:41 +0900

    member-list can selectable, add hover reaction

--- front_web/src/app/components/member-list/member-list.component.ts
+++ front_web/src/app/components/member-list/member-list.component.ts
@@ -5,14 +5,27 @@ import { NameTagComponent } from '../name-tag/name-tag.component';
 import { Member } from '../../interfaces/member';
 import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzWrap } from 'ng-zorro-antd/flex';
+import { ID } from '../../interfaces/id';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [NzFlexModule, NzCardModule, NzListModule, NameTagComponent],
+  imports: [CommonModule,NzFlexModule, NzCardModule, NzListModule, NameTagComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.less'
 })
 export class MemberListComponent {
   @Input() members: Member[] = []
+  @Input() onMemberSelected: (member: Member) => void = () => {}
+  
+  highlightId: ID = 0
+
+  onMouseOver(item: Member) {
+    this.highlightId = item.id
+  }
+
+  onMouseLeave() {
+    this.highlightId = 0
+  }
 }
