d1f5d53dc86bb3c56649e87c1c5ecadb274ea3bd
SoKyungHyun <valentine92exp@gmail.com>
2024-07-01 14:39:55 +0900

    apply member-list component

--- front_web/src/app/components/company-members-card/company-members-card.component.ts
+++ front_web/src/app/components/company-members-card/company-members-card.component.ts
@@ -2,18 +2,21 @@ import { Component, Input } from '@angular/core';
 import { NzCardComponent } from 'ng-zorro-antd/card';
 import { ModalButtonComponent } from '../modal-button/modal-button.component';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NameTagComponent } from '../name-tag/name-tag.component';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { FoldingCardComponent } from '../folding-card/folding-card.component';
 import { Member } from '../../interfaces/member';
 import { CompanyService } from '../../services/company.service';
 import { Auth } from '../../interfaces/auth';
+import { MemberListComponent } from '../member-list/member-list.component';
 
 @Component({
   selector: 'app-company-members-card',
   standalone: true,
-  imports: [NzCardComponent, ModalButtonComponent, NzIconModule, NameTagComponent, NzListModule, NzGridModule, FoldingCardComponent],
+  imports: [
+    NzCardComponent, ModalButtonComponent, NzIconModule,NzListModule, NzGridModule, 
+    FoldingCardComponent, MemberListComponent
+  ],
   templateUrl: './company-members-card.component.html',
   styleUrl: './company-members-card.component.less'
 })
--- front_web/src/app/components/group-manage-card/group-card/group-card.component.ts
+++ front_web/src/app/components/group-manage-card/group-card/group-card.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzListModule } from 'ng-zorro-antd/list';
-import { NameTagComponent } from '../../name-tag/name-tag.component';
 import { GroupTagComponent } from '../group-tag/group-tag.component';
 import { Group } from '../../../interfaces/group';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { ModalButtonComponent } from '../../modal-button/modal-button.component';
+import { MemberListComponent } from '../../member-list/member-list.component';
 
 @Component({
   selector: 'app-group-card',
   standalone: true,
-  imports: [ModalButtonComponent,NzListModule, NzGridModule, NameTagComponent, NzListModule, GroupTagComponent, NzCardModule],
+  imports: [
+    NzListModule, NzGridModule, NzListModule, NzCardModule,
+    GroupTagComponent,ModalButtonComponent, MemberListComponent
+  ],
   templateUrl: './group-card.component.html',
   styleUrl: './group-card.component.less'
 })
