98dba52c203d10589bc8a5f1ac07e6e7731f9f76
SoKyungHyun <valentine92exp@gmail.com>
2024-07-04 09:47:04 +0900

    comapny service subscribe auth

--- front_web/src/app/services/company.service.ts
+++ front_web/src/app/services/company.service.ts
@@ -8,6 +8,7 @@ import { AxiosInstanceService } from './axios-instance.service';
 import { Organize } from '../interfaces/organize';
 import { AuthService } from './auth.service';
 import { Auth } from '../interfaces/auth';
+import { Subscription } from 'rxjs';
 
 
 @Injectable({
@@ -15,40 +16,64 @@ import { Auth } from '../interfaces/auth';
 })
 export class CompanyService {
 
-  auth : Auth | null;
-  companyId : ID;
+  private auth : Auth | null = null;
+  private companyId : ID = 0;
   private axiosInstance: AxiosInstance;
+  private authSubscription: Subscription
 
   constructor(private authService: AuthService) {
     this.axiosInstance = new AxiosInstanceService().getAxiosInstance()
-    this.auth = this.authService.getAuth();
-    if(!this.auth){
-      throw new Error('로그인이 필요합니다')
+    this.authSubscription = this.authService.auth$.subscribe(auth => {
+      this.auth = auth;
+      if(auth){
+        this.companyId = auth.company_id;
+      }else {
+        this.companyId = 0;
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.authSubscription){
+      this.authSubscription.unsubscribe();
     }
-    this.companyId = this.auth.company_id;
+  }
+
+  private checkAuth() {
+    if (!this.auth) {
+      throw new Error('로그인이 필요합니다');
+    }
+    if (this.companyId==0) {
+      throw new Error('회사 ID를 찾을 수 없습니다');
+    }
   }
 
   async getCompanyInfo(): Promise<Company> {
+    this.checkAuth();
     const response = await this.axiosInstance.get<Company>(`/companies/${this.companyId}`);
     return response.data
   }
 
   async getCompanyMembers(): Promise<Member[]> {
+    this.checkAuth();
     const response = await this.axiosInstance.get<Member[]>(`/companies/${this.companyId}/members`);
     return response.data
   }
 
   async getCompanyMembersWithKeyword(keyword : string): Promise<Member[]> {
+    this.checkAuth();
     const response = await this.axiosInstance.get<Member[]>(`/companies/${this.companyId}/members/search?keyword=${keyword}`);
     return response.data
   }
 
   async getCompanyGroups(): Promise<Group[]> {
+    this.checkAuth();
     const response = await this.axiosInstance.get<Group[]>(`/companies/${this.companyId}/groups`);
     return response.data
   }
 
   async getCompanyOrganizes(): Promise<Organize> {
+    this.checkAuth();
     const response = await this.axiosInstance.get<Organize>(`/companies/${this.companyId}/organizes`);
     return response.data
   }
