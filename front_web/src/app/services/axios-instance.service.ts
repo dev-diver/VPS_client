15f424166f9452752fea97dea923d6db415278bb
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-07-08 22:35:48 +0900

    로@

--- front_web/src/app/services/axios-instance.service.ts
+++ front_web/src/app/services/axios-instance.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import axios, { AxiosInstance } from 'axios';
 import { environment } from '../../environments/environment';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
