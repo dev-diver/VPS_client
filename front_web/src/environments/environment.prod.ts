cb84c7ad074f4aea34a5efdc74b12a21971fd12d
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-07-09 05:32:57 +0900

    hook

--- front_web/src/environments/environment.prod.ts
+++ front_web/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
-  production: true,
-  apiUrl: `http://${window.location.hostname}:3000/api`
+  production: false,
+  apiUrl: `http://${window.location.hostname}:3000/api`,
+  hookUrl: `http://${window.location.hostname}:5000/`
 };
